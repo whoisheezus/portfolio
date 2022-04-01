0d2ea54f635b415bd6294c848901c3d2b23c5b92
Nikita Myshkin <whataboutnicolas@yandex.ru>
2022-04-01 12:29:24 +0300

    projects js update

--- ru/projects/js/projects.js
+++ ru/projects/js/projects.js
@@ -1,183 +1,19 @@
-function setBg1(){
-    document.getElementById("mainBg").style.backgroundColor = '#c3203c';
-    document.getElementById("mainOverlay").style.backgroundColor = '#c3203c';
+function changeLayout(newMainBg, newTitle, newBlock){
+    document.getElementById('mainBg').style.backgroundColor = newMainBg;
+    document.getElementById("mainOverlay").style.backgroundColor = newMainBg;
     document.getElementById("mainOverlay").style.opacity = '0.7';
     document.getElementById("mainOverlay").style.zIndex = '2';
     document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "отваал";
-    document.getElementById("kurt1").style.zIndex = '10';
-
+    document.getElementById("title").textContent = newTitle;
+    document.getElementById(newBlock).style.zIndex = '10';
 }
 
-function returnBg1(){
-
+function returnLayout(newBlock){
     document.getElementById("mainBg").style.backgroundColor = '#060606';
     document.getElementById("mainOverlay").style.backgroundColor = '#060606';
     document.getElementById("mainOverlay").style.opacity = '0';
     document.getElementById("mainOverlay").style.zIndex = '0';
     document.getElementById("title").style.zIndex = '11';
     document.getElementById("title").textContent = "проекты и сюжеты";
-    document.getElementById("kurt1").style.zIndex = '1';
-
-}
-
-function setBg2(){
-    document.getElementById("mainBg").style.backgroundColor = '#515877';
-    document.getElementById("mainOverlay").style.backgroundColor = '#515877';
-    document.getElementById("mainOverlay").style.opacity = '0.7';
-    document.getElementById("mainOverlay").style.zIndex = '2';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "интурист";
-    document.getElementById("kurt2").style.zIndex = '10';
-
-}
-
-function returnBg2(){
-
-    document.getElementById("mainBg").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.opacity = '0';
-    document.getElementById("mainOverlay").style.zIndex = '0';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "проекты и сюжеты";
-    document.getElementById("kurt2").style.zIndex = '1';
-
-}
-
-function setBg3(){
-    document.getElementById("mainBg").style.backgroundColor = '#dfb3a1';
-    document.getElementById("mainOverlay").style.backgroundColor = '#dfb3a1';
-    document.getElementById("mainOverlay").style.opacity = '0.7';
-    document.getElementById("mainOverlay").style.zIndex = '2';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "postcards of overexposed healing";
-    document.getElementById("kurt3").style.zIndex = '10';
-
-}
-
-function returnBg3(){
-
-    document.getElementById("mainBg").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.opacity = '0';
-    document.getElementById("mainOverlay").style.zIndex = '0';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "проекты и сюжеты";
-    document.getElementById("kurt3").style.zIndex = '1';
-
-}
-
-function setBg4(){
-    document.getElementById("mainBg").style.backgroundColor = '#992315';
-    document.getElementById("mainOverlay").style.backgroundColor = '#992315';
-    document.getElementById("mainOverlay").style.opacity = '0.7';
-    document.getElementById("mainOverlay").style.zIndex = '2';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "шхд: зима";
-    document.getElementById("kurt4").style.zIndex = '10';
-
-}
-
-function returnBg4(){
-
-    document.getElementById("mainBg").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.opacity = '0';
-    document.getElementById("mainOverlay").style.zIndex = '0';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "проекты и сюжеты";
-    document.getElementById("kurt4").style.zIndex = '1';
-
-}
-
-function setBg5(){
-    document.getElementById("mainBg").style.backgroundColor = '#e34b24';
-    document.getElementById("mainOverlay").style.backgroundColor = '#e34b24';
-    document.getElementById("mainOverlay").style.opacity = '0.7';
-    document.getElementById("mainOverlay").style.zIndex = '2';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "oqjav";
-    document.getElementById("kurt5").style.zIndex = '10';
-
-}
-
-function returnBg5(){
-
-    document.getElementById("mainBg").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.opacity = '0';
-    document.getElementById("mainOverlay").style.zIndex = '0';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "проекты и сюжеты";
-    document.getElementById("kurt5").style.zIndex = '1';
-
-}
-
-function setBg6(){
-    document.getElementById("mainBg").style.backgroundColor = '#c38d9b';
-    document.getElementById("mainOverlay").style.backgroundColor = '#c38d9b';
-    document.getElementById("mainOverlay").style.opacity = '0.7';
-    document.getElementById("mainOverlay").style.zIndex = '2';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "i see you";
-    document.getElementById("kurt6").style.zIndex = '10';
-
-}
-
-function returnBg6(){
-
-    document.getElementById("mainBg").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.opacity = '0';
-    document.getElementById("mainOverlay").style.zIndex = '0';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "проекты и сюжеты";
-    document.getElementById("kurt6").style.zIndex = '1';
-
-}
-
-function setBg7(){
-    document.getElementById("mainBg").style.backgroundColor = '#463a5f';
-    document.getElementById("mainOverlay").style.backgroundColor = '#463a5f';
-    document.getElementById("mainOverlay").style.opacity = '0.7';
-    document.getElementById("mainOverlay").style.zIndex = '2';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "cph";
-    document.getElementById("kurt7").style.zIndex = '10';
-
-}
-
-function returnBg7(){
-
-    document.getElementById("mainBg").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.opacity = '0';
-    document.getElementById("mainOverlay").style.zIndex = '0';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "проекты и сюжеты";
-    document.getElementById("kurt7").style.zIndex = '1';
-
-}
-
-function setBg8(){
-    document.getElementById("mainBg").style.backgroundColor = '#0b86e1';
-    document.getElementById("mainOverlay").style.backgroundColor = '#0b86e1';
-    document.getElementById("mainOverlay").style.opacity = '0.7';
-    document.getElementById("mainOverlay").style.zIndex = '2';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "металлострой";
-    document.getElementById("kurt8").style.zIndex = '10';
-
-}
-
-function returnBg8(){
-
-    document.getElementById("mainBg").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.backgroundColor = '#060606';
-    document.getElementById("mainOverlay").style.opacity = '0';
-    document.getElementById("mainOverlay").style.zIndex = '0';
-    document.getElementById("title").style.zIndex = '11';
-    document.getElementById("title").textContent = "проекты и сюжеты";
-    document.getElementById("kurt8").style.zIndex = '1';
-
+    document.getElementById(newBlock).style.zIndex = '1';
 }
